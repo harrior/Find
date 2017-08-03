ed569e4e1de2ad5d578bb6aac02b958279e92f0b
harrior <harrior@gmail.com>
2017-08-03 08:38:30 +0300

    Add file to prj

---
+++ FileFinder/Unit1.dfm
@@ -0,0 +1,95 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 377
+  ClientWidth = 702
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnClose = FormClose
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object ResultGrid: TStringGrid
+    Left = 0
+    Top = 44
+    Width = 505
+    Height = 326
+    ColCount = 1
+    Ctl3D = True
+    DefaultColWidth = 500
+    FixedCols = 0
+    RowCount = 1
+    FixedRows = 0
+    ParentCtl3D = False
+    TabOrder = 0
+    OnDblClick = ResultGridDblClick
+  end
+  object Panel1: TPanel
+    Left = 0
+    Top = 0
+    Width = 505
+    Height = 41
+    Margins.Left = 0
+    Margins.Top = 0
+    Margins.Right = 0
+    Margins.Bottom = 0
+    TabOrder = 1
+    object FilenameLabel: TLabel
+      Left = 24
+      Top = 14
+      Width = 54
+      Height = 13
+      Caption = #1048#1084#1103' '#1092#1072#1081#1083#1072
+    end
+    object FindEdit: TEdit
+      Left = 104
+      Top = 11
+      Width = 281
+      Height = 21
+      TabOrder = 0
+      Text = 'biblio.nsi'
+      OnKeyPress = FindEditKeyPress
+    end
+    object FindButton: TButton
+      Left = 416
+      Top = 8
+      Width = 75
+      Height = 25
+      Caption = #1053#1072#1081#1090#1080
+      TabOrder = 1
+      OnClick = FindButtonClick
+    end
+  end
+  object Panel2: TPanel
+    Left = 508
+    Top = 0
+    Width = 185
+    Height = 370
+    Caption = 'Panel2'
+    TabOrder = 2
+    object Memo1: TMemo
+      Left = 3
+      Top = 40
+      Width = 177
+      Height = 321
+      Lines.Strings = (
+        'Memo1')
+      TabOrder = 0
+    end
+    object RefreshButton: TButton
+      Left = 46
+      Top = 9
+      Width = 99
+      Height = 25
+      Caption = #1054#1073#1085#1086#1074#1080#1090#1100' '#1080#1085#1076#1077#1082#1089
+      TabOrder = 1
+      OnClick = RefreshButtonClick
+    end
+  end
+end
---
+++ FileFinder/Unit1.pas
@@ -0,0 +1,134 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Grids, Vcl.ExtCtrls, ShellAPI;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    FindEdit: TEdit;
+    FilenameLabel: TLabel;
+    ResultGrid: TStringGrid;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    RefreshButton: TButton;
+    FindButton: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure RefreshButtonClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure FindButtonClick(Sender: TObject);
+    procedure ResultGridDblClick(Sender: TObject);
+    procedure FindEditKeyPress(Sender: TObject; var Key: Char);
+
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  drives: TStringList;
+  filelist:TStringList;
+
+
+
+implementation
+
+{$R *.dfm}
+
+procedure RefreshDirList(Path: String; listofpaths: TStringList);
+var
+ F: TSearchRec;
+begin
+  if Path[Length(Path)] <> '\' then Path := Path + '\';
+  if FindFirst(Path+'*.*', faAnyFile, F) = 0 then
+  begin
+   repeat
+    Application.ProcessMessages;
+     if (F.Attr and faDirectory) <> faDirectory then
+      listofpaths.Add(Path + F.Name)
+     else if (F.Name <> '..') and (F.Name <> '.') then
+      RefreshDirList(Path + F.Name + '', listofpaths);
+    until FindNext(F) <> 0;
+    FindClose(F);
+  end;
+end;
+
+procedure TForm1.FindButtonClick(Sender: TObject);
+var
+  i: Integer;
+  FindList: TStringList;
+  FindStr:String;
+begin
+FindList := TStringList.Create;
+FindStr := AnsiUpperCase(FindEdit.Text);
+for i := 0 to filelist.Count-1 do
+  begin
+    if pos(FindStr,AnsiUpperCase(filelist[i]))<>0 then
+      FindList.Add(filelist[i])
+  end;
+  memo1.Lines.Add('Поиск завершен.');
+ResultGrid.RowCount:=FindList.Count;
+for i := 0 to FindList.Count-1 do
+  ResultGrid.Cells[0,i]:=FindList[i];
+
+end;
+
+procedure TForm1.FindEditKeyPress(Sender: TObject; var Key: Char);
+begin
+if Key = #13 then
+  FindButton.Click;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+filelist.Free;
+drives.Free;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  c: char;
+begin
+  filelist := TStringList.Create;
+
+  drives := TStringList.Create;
+  for c := 'A' to 'Z' do
+    begin
+    if (GetDriveType(PChar(c+':\')) <> 1) and (GetDriveType(PChar(c+':\')) <> 0) then
+      begin
+      drives.Add(c+':');
+      memo1.Lines.Add(c);
+      end;
+    end;
+
+end;
+
+procedure TForm1.RefreshButtonClick(Sender: TObject);
+var
+I: Integer;
+begin
+filelist.Clear;
+for I := 0 to drives.Count-1 do
+  begin
+  RefreshDirList(drives[i],filelist);
+  memo1.Lines.Add(inttostr(filelist.Count));
+  end;
+//filelist.SaveToFile('E:\test.txt');
+end;
+
+procedure TForm1.ResultGridDblClick(Sender: TObject);
+var
+Path,Filename : String;
+begin
+Path := ResultGrid.Cells[0,ResultGrid.Row];
+
+if FileExists(Path) then
+  ShellExecute(Application.Handle, 'OPEN', 'EXPLORER', PWideChar('/select, ' + Path), '', SW_NORMAL);
+end;
+
+end.
